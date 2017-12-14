85adb5b278f9e167059305d90338c53cf2b2ab4a
jaja <jaja@example.com>
2017-12-15 00:26:40 +0800

    ADD Insert NoteMain

--- src/app/myapp/notes/notes.go
+++ src/app/myapp/notes/notes.go
@@ -14,10 +14,22 @@ func GetNoteInfo(c *gin.Context) {
 	c.JSON(200, note.GetNotes())
 }
 
+//Insert NoteMain Info
+func InsertNoteMain(c *gin.Context) {
+	log.Println("IN")
+	var result noteRepo.NoteMainModel
+	if c.BindJSON(&result) == nil {
+		log.Println("OK")
+		log.Println(result)
+		note.InsertNoteMain(result)
+	}
+	//c.String(200, "Success")
+}
+
 //InsertNoteInfo is Insert Note Info
 func InsertNoteInfo(c *gin.Context) {
 	log.Println("IN")
-	var result noteRepo.Note
+	var result noteRepo.NoteModel
 	if c.BindJSON(&result) == nil {
 		log.Println("OK")
 		log.Println(result)
--- src/app/myapp/repository/noteRepo/noteRepo.go
+++ src/app/myapp/repository/noteRepo/noteRepo.go
@@ -7,13 +7,19 @@ import (
 	mysql "github.com/go-sql-driver/mysql"
 )
 
-// Note is Note data struct
-type Note struct {
+// Note data model struct
+type NoteModel struct {
 	NoteColor     int     `json:"NoteColor"`
 	NoteContent   string  `json:"Notecontent"`
 	NotePositionX float32 `json:"NotePositionX"`
 	NotePositionY float32 `json:"NotePositionY"`
 	FK_NoteKey    string  `json:"FK_NoteKey"`
+}
+
+// NoteMain data model struct
+type NoteMainModel struct {
+	NoteName string `json:"NoteName"`
+	NoteKey  string `json:"NoteKey"`
 }
 
 // NoteRepository is Public Struct for Use
@@ -32,13 +38,16 @@ func init() {
 }
 
 // GetNotes will return mutiple Note data
-func (noteRep *NoteRepository) GetNotes() []Note {
+func (noteRep *NoteRepository) GetNotes() []NoteModel {
 	db, err := sql.Open("mysql", config.FormatDSN())
 	checkErr(err)
-	rows, err := db.Query("SELECT NoteColor,NoteContent,NotePositionX,NotePositionY FROM NotesList")
+	rows, err := db.Query(`SELECT 
+		NoteColor,NoteContent
+		,NotePositionX,NotePositionY 
+		FROM NotesList`)
 	checkErr(err)
 	defer rows.Close()
-	var notes []Note
+	var notes []NoteModel
 	var (
 		NoteColor     int
 		NoteContent   string
@@ -48,7 +57,7 @@ func (noteRep *NoteRepository) GetNotes() []Note {
 	for rows.Next() {
 		err := rows.Scan(&NoteColor, &NoteContent, &NotePositionX, &NotePositionX)
 		checkErr(err)
-		notes = append(notes, Note{
+		notes = append(notes, NoteModel{
 			NoteColor:     NoteColor,
 			NoteContent:   NoteContent,
 			NotePositionX: NotePositionX,
@@ -57,11 +66,26 @@ func (noteRep *NoteRepository) GetNotes() []Note {
 	return notes
 }
 
-// InsertNote is insert note data
-func (noteRep *NoteRepository) InsertNote(s Note) {
+// InsertNoteMain is insert note main data
+func (noteRep *NoteRepository) InsertNoteMain(s NoteMainModel) {
 	db, err := sql.Open("mysql", config.FormatDSN())
 	checkErr(err)
-	stmt, err := db.Prepare("INSERT NotesList SET NoteColor=? , NoteContent=? , NotePositionX=? , NotePositionY=?")
+	stmt, err := db.Prepare(`INSERT NotesMain
+		SET NoteName=? , NoteKey=? `)
+	checkErr(err)
+	res, err := stmt.Exec(s.NoteName, s.NoteKey)
+	checkErr(err)
+	fmt.Println("Insert Success")
+	fmt.Println(res.RowsAffected())
+}
+
+// InsertNote is insert note data
+func (noteRep *NoteRepository) InsertNote(s NoteModel) {
+	db, err := sql.Open("mysql", config.FormatDSN())
+	checkErr(err)
+	stmt, err := db.Prepare(`INSERT NotesList 
+		SET NoteColor=? , NoteContent=? , 
+		NotePositionX=? , NotePositionY=?`)
 	checkErr(err)
 	res, err := stmt.Exec(s.NoteColor, s.NoteContent, s.NotePositionX, s.NotePositionY)
 	checkErr(err)
